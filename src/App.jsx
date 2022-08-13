c7753747fab754b04c8a0ddfa40d973dbb7cee00
zeshan7061 <zeshan7061@gmail.com>
2022-08-13 22:22:31 +0500

    adds testimonials, cta and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import React from 'react'
-import './App.css'
+import styles from './style'
+import {
+	Navbar,
+	Hero,
+	Business,
+	Billing,
+	Clients,
+	CardDeal,
+	CTA,
+	Stats,
+	Testimonials,
+	Footer,
+} from './components/index'
 
 const App = () => (
-	<div className='App'>
-		<div>
-			<div>Navbar</div>
+	<div className='bg-primary w-full overflow-hidden'>
+		<div className={`${styles.paddingX} ${styles.flexCenter}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Navbar />
+			</div>
 		</div>
 
-		<div>
-			<div>Hero</div>
+		<div className={`bg-primary ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Hero />
+			</div>
 		</div>
 
-		<div>
-			<div>
-				Stats
-				Business
-				Billing
-				CardDeal
-				Testimonials
-				Clients
-				CTA
-				Footer
+		<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Stats />
+				<Businesss />
+				<Billing />
+				<CardDeal />
+				<Testimonials />
+				<Clients />
+				<CTA />
+				<Footer />
 			</div>
 		</div>
 	</div>
