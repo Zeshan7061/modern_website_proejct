e54a5d8de5233cf5997e6106a2f6bfc85214345c
zeshan7061 <zeshan7061@gmail.com>
2022-08-13 22:10:46 +0500

    initial app setup with

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css'
+
+const App = () => (
+	<div className='App'>
+		<div>
+			<div>Navbar</div>
+		</div>
+
+		<div>
+			<div>Hero</div>
+		</div>
+
+		<div>
+			<div>
+				Stats
+				Business
+				Billing
+				CardDeal
+				Testimonials
+				Clients
+				CTA
+				Footer
+			</div>
+		</div>
+	</div>
+)
+
+export default App
